8742337de8c371a2af7928154d137c93c9581632
Nishant <nishant2feb@gmail.com>
2018-02-28 13:07:30 +0530

    3
    Starting text analytics

--- Analysis.R
+++ Analysis.R
@@ -15,3 +15,41 @@ summary_by_country = Loan %>%
 summary_by_country = summary_by_country%>%
   arrange(no_loan)
 
+summary(summary_by_country)
+
+summary_by_country%>%
+  filter(no_loan <= 363)%>%
+ggplot(aes(x = no_loan, y = LoanAmount, col = country))+
+  geom_point()+
+  theme_classic()
+
+summary_by_country%>%
+  filter(no_loan <= 2313 && no_loan > 363)%>%
+  ggplot(aes(x = no_loan, y = LoanAmount, col = country))+
+  geom_point()+
+  theme_classic()
+
+summary_by_country%>%
+  filter(no_loan <= 6687 && no_loan > 2313)%>%
+  ggplot(aes(x = no_loan, y = LoanAmount, col = country))+
+  geom_point()+
+  theme_classic()
+
+summary_by_country%>%
+  filter(no_loan > 6687)%>%
+  ggplot(aes(x = no_loan, y = LoanAmount, col = country))+
+  geom_point()+
+  theme_classic()
+
+## Analysing data of text like sector activity and use
+library(qdap)
+library(tm)
+
+activity_text = (Loan$activity)
+tm_map(activity_text, removeNumbers)
+tm_map(activity_text, removePunctuation)
+
+activity_wfm = wfm(Loan$activity)
+
+
+
